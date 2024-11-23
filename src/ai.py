20e234ecf0961ab13ac21e2808e3275326ad2660
Istvan Dano <istvan.dano@dek-sk.com>
2024-11-23 19:46:58 +0100

    npm

--- src/ai.py
+++ src/ai.py
@@ -1,15 +1,6 @@
 from openai import OpenAI
 client = OpenAI()
 
-# def get_key():
-#     try:
-#         with open("config.json", 'r') as file:
-#             config_data = json.load(file)
-#             return config_data.get("key")
-#     except:
-#         print("openai.py -> get_key: error opening/reading the file")
-
-
 def ask(str, file_path=None):
     '''
     ask gpt a question
@@ -32,8 +23,3 @@ def ask(str, file_path=None):
         ]
     )
     return completion.choices[0].message.content
-
-
-
-
-# ask(input("Ask smth: "))
