d6c87ffe2106a05c3dfda786fff943138bb063c2
Istvan Dano <istvan.dano@dek-sk.com>
2024-11-23 19:23:16 +0100

    update

--- src/main.py
+++ src/main.py
@@ -29,7 +29,10 @@ def save_tree_structure_to_file(root_folder, output_file):
         file.write("\n".join(tree))
     print(f"Tree structure saved to {output_file}")
 
+counter = 0
+
 def get_path(u_input):
+    counter += 1
     # get tree structure
     tree = get_tree_structure(f"{os.getcwd()}/data")
     ai_answer = ask(f"Can u find the path to the file mentioned in this sentence: {u_input}? here is the folder structure: {tree}. Print it out like this targetfolder='path'")
@@ -42,15 +45,20 @@ def get_path(u_input):
             print(f"Extracted path: {path}")
             return path
     print("Path not found in the response.")
+    if counter == 10:
+        return None
     return get_path(u_input)
 
 
 
-def execute():
-    u_input = input("> ")
+def execute(u_input):
+    # u_input = input("> ")
     # path to get file from
-    path ="data/" + get_path(u_input)
-    
+    if path :
+        path ="data/" + get_path(u_input)
+    else:
+        print("File with similar name not found")
+
     # plot graph using specified file
     from generate_graph import generate_graphs
     generate_graphs(path, u_input)
